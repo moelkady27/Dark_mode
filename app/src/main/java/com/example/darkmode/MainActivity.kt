0d8c9ca1b77d754b69fc25f936c8184e522dae68
moelkady27 <mohamedkhaledelkady11@gmail.com>
2023-11-14 15:00:25 +0200

    intial commit

---
+++ app/src/main/java/com/example/darkmode/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.darkmode
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.imageView
+import kotlinx.android.synthetic.main.activity_main.switch1
+import kotlinx.android.synthetic.main.activity_main.textView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView.setOnClickListener {
+            Toast.makeText(this@MainActivity , "Image" , Toast.LENGTH_LONG).show()
+            Log.e("error is ", "image")
+        }
+
+        textView.setOnClickListener {
+            Toast.makeText(this@MainActivity , "Text" , Toast.LENGTH_LONG).show()
+            Log.e("error is ", "text")
+        }
+
+        val sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val nightMode = sharedPreferences.getBoolean("night",false)
+
+        if (nightMode){
+            switch1.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+
+        switch1.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            if (!isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("night", false)
+                editor.apply()
+
+            }else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("night", true)
+                editor.apply()
+            }
+        }
+    }
+}
